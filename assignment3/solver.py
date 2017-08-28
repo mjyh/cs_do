aab72148dc6598cad505b7bfefdfaebd1ce3e81f
mjyh <michael.han@futureadvisor.com>
2017-08-28 09:35:19 -0700

    fix assignment 3

--- assignment3/solver.py
+++ assignment3/solver.py
@@ -21,12 +21,12 @@ class GreedySolver:
         self.reset()
     
     def reset(self):
-        self.nodes_colored = np.zeros(0, dtype=np.int8)
-        self.colors_used = np.zeros(0, dtype=np.int8)
-        self.solution = np.full([self.node_count], np.nan, dtype=np.int8)
+        self.nodes_colored = np.zeros(0, dtype=np.int)
+        self.colors_used = np.zeros(0, dtype=np.int)
+        self.solution = np.full([self.node_count], -1, dtype=np.int)
         
         # build edge matrix
-        self.edge_mat = np.zeros([self.node_count, self.node_count], dtype=np.int8)
+        self.edge_mat = np.zeros([self.node_count, self.node_count], dtype=np.int)
         
         for edge_pair in self.edge_list:
             self.edge_mat[edge_pair[0], edge_pair[1]] = 1
@@ -39,24 +39,50 @@ class GreedySolver:
         self.con_mat = np.full([self.node_count, self.node_count], np.nan)
     
     def find_next_node_most_initial_constraints(self):
+        num_nodes_colored = len(self.nodes_colored)
+        
+        edge_counts = self.edge_mat.sum(axis=0)
+
+        edge_counts_paired = np.stack([ np.arange(0, self.node_count), edge_counts ])
+        temp_copy = edge_counts_paired.copy()
+        temp_copy[1,:] = -temp_copy[1,:]
+        
+        edge_counts_paired = edge_counts_paired[:, np.lexsort(temp_copy)]
+        
+        node = edge_counts_paired[0,num_nodes_colored]
+        
+        return node
         
     def find_next_node_most_constrained(self):
-        num_constraints = np.count_nonzero(~np.isnan(self.con_mat), axis=0)-self.total_cons
+        num_constraints = np.count_nonzero(~np.isnan(self.con_mat), axis=0) - self.total_cons
         num_constraints_paired = np.stack([ np.arange(0, self.node_count), num_constraints ])
-        uncolored_nodes = np.setdiff1d( np.arange(0, self.node_count, dtype=np.int8), self.nodes_colored )
+        uncolored_nodes = np.setdiff1d( np.arange(0, self.node_count, dtype=np.int), self.nodes_colored )
         available_num_constraints_paired = num_constraints_paired[:, uncolored_nodes]
         available_most_constrained = available_num_constraints_paired[:, np.lexsort(available_num_constraints_paired)]
         node = available_most_constrained[0][0]
             
         return node
     
+    def available_colors(self, node):
+        return np.setdiff1d(self.colors_used, self.con_mat[node])
+    
     def choose_color_min_value(self, node):
-        available_colors = np.setdiff1d(self.colors_used, self.con_mat[node])
+        available_colors = self.available_colors(node)
         if len(available_colors) == 0:
             color = len(self.colors_used)
             self.colors_used = np.append(self.colors_used, color) 
         else:
             color = np.nanmin(available_colors)
+            
+        return color
+
+    def choose_color_random(self, node):
+        available_colors = self.available_colors(node)
+        if len(available_colors) == 0:
+            color = len(self.colors_used)
+            self.colors_used = np.append(self.colors_used, color) 
+        else:
+            color = np.random.choice(available_colors)
             
         return color
     
@@ -66,13 +92,15 @@ class GreedySolver:
         # assign number to solution
         for ind in range(self.node_count):
             # find node
-            node = self.find_next_node_most_constrained()
+            node = self.find_next_node_most_initial_constraints()
+            # node = self.find_next_node_most_constrained()
             
             self.nodes_colored = np.append(self.nodes_colored, node)
             
             # find color
-            color = self.choose_color_min_value(node)
-    
+            # color = self.choose_color_min_value(node)
+            color = self.choose_color_random(node)
+             
             # set color and update constraints
             self.con_mat[node, self.edge_mat[node]==1] = color
             self.con_mat[self.edge_mat[node]==1, node] = color
@@ -112,12 +140,22 @@ import sys
 
 if __name__ == '__main__':
     import sys
-    if len(sys.argv) > 1:
-        file_location = sys.argv[1].strip()
-        file_location = r'data//gc_20_3'
-        with open(file_location, 'r') as input_data_file:
-            input_data = input_data_file.read()
-        print(solve_it(input_data))
+    if len(sys.argv) > 0:
+        #file_location = sys.argv[1].strip()
+        p1 = r'./data/gc_50_3'
+        p2 = r'./data/gc_70_7'
+        p3 = r'./data/gc_100_5'
+        p4 = r'./data/gc_250_9'
+        p5 = r'./data/gc_500_1'
+        p6 = r'./data/gc_1000_5'
+
+        for file_location in [p1, p2, p3, p4, p5, p6]:
+            #file_location = p1
+            
+            with open(file_location, 'r') as input_data_file:
+                input_data = input_data_file.read()
+            
+            print(solve_it(input_data))
     else:
         print('This test requires an input file.  Please select one from the data directory. (i.e. python solver.py ./data/gc_4_1)')
 
